dc8c71da9ed86277c2910c57395c13e6a6763bdd
KIM HONGIL <clroyan01@gmail.com>
2019-11-07 19:06:48 +0900

    Create pagination project

---
+++ src/test/java/boot/jpa/pagination/PaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.jpa.pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
